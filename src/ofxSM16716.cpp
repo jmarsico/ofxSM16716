fe634b1568ed6d925b7958b8c24181b5b7ac1e99
Jake <jmarsico@gmail.com>
2015-04-21 16:51:40 +0000

    init with num lights

--- src/ofxSM16716.cpp
+++ src/ofxSM16716.cpp
@@ -14,7 +14,7 @@ ofxSM16716::ofxSM16716() {
 }
 
 
-bool ofxSM16716::init(){
+bool ofxSM16716::init(int num_pix){
   
   int setupResult = wiringPiSetup();
 
@@ -26,12 +26,11 @@ bool ofxSM16716::init(){
     return false;
   }
 
+  
+  pixels.resize(num_pix);
 
-  // setup Arduino with data pins for each strip
-  for (int k = 0; k < NUM_STRIPS; k++) {
-    pinMode(DATA_PIN + k, OUTPUT);
-    digitalWrite(DATA_PIN + k, LOW);
-  }
+  pinMode(DATA_PIN, OUTPUT);
+  digitalWrite(DATA_PIN, LOW);
   // setup Arduino for clock pin
   pinMode(CLOCK_PIN, OUTPUT);
   digitalWrite(CLOCK_PIN, LOW);
@@ -49,21 +48,21 @@ ofxSM16716::~ofxSM16716() {
 
 // set a pixel in the RGB array in the strip layout using a single 32-bit int for the color
 // masks with 00FFFFFF since color is 24-bit value, thus this sets uppermost byte to zeros
-void ofxSM16716::set_pixel(uint8_t strip, uint8_t index, uint32_t color) {
-  pixels[strip][index] = color & 0x00FFFFFF;
+void ofxSM16716::set_pixel(uint8_t index, uint32_t color) {
+  pixels[index] = color & 0x00FFFFFF;
 }
 
 // set a pixel in the RBG array in the strip layout using separate ints for R, G, and B
 // creates a 32-bit uint from RGB values, then calls set_pixel
 // bytes are red, green, blue from left to right
 // note: some implementations have this backwards
-void ofxSM16716::set_pixel_rgb(uint8_t strip, uint8_t index, uint32_t r, uint32_t g, uint32_t b) {
+void ofxSM16716::set_pixel_rgb(uint8_t index, uint32_t r, uint32_t g, uint32_t b) {
   
   if(r > 255) r = 255;
   if(g > 255) g = 255;
   if(b > 255) b = 255;
 
-  set_pixel(strip, index, b | (g << 8) | (r << 16));
+  set_pixel(index, b | (g << 8) | (r << 16));
 }
 
 
@@ -75,19 +74,19 @@ void ofxSM16716::toggle_clock() {
 }
 
 // write a single pixel of given index in strip layout from RGB array to a given strip
-void ofxSM16716::write_pixel(uint8_t strip, uint8_t i) {
+void ofxSM16716::write_pixel(uint8_t i) {
   // mask has a 1 in bit 25, zero everywhere else
   const uint32_t MASK = ((uint32_t)(1) << 24);
   // puts the bit 1 in front of the RGB value
   // so p (in binary) looks like 1 + red (8 bits) + green (8 bits) + blue (8 bits)
   // basically, we are putting a bit 1 between each set of 24 RGB bits
-  uint32_t p = pixels[strip][i] | MASK;
+  uint32_t p = pixels[i] | MASK;
   // send bits from most to least significant, we are sending 25 bits total
   int j = 25;
   while (j--) {
     // send the bit in the 25th position (from the least significant end)
     // write HIGH if bit is 1, LOW if bit is 0
-    digitalWrite(DATA_PIN + strip, (p & MASK) ? HIGH : LOW);
+    digitalWrite(DATA_PIN, (p & MASK) ? HIGH : LOW);
     // toggle clock to "push" each bit one down the strip
     toggle_clock();
     // shift p to the left by one so the next bit we want is in the 25th position
@@ -95,28 +94,28 @@ void ofxSM16716::write_pixel(uint8_t strip, uint8_t i) {
   }
 }
 
-// write pixels of given index in strip layout from RGB array to all strips
-// this has (theoretical) advantage that a single toggle_clock() pushes data down all strips at once
-void ofxSM16716::write_pixel_all_strips(uint8_t pos) {
-  // mask has a 1 in bit 25, zero everywhere else
-  const uint32_t MASK = ((uint32_t)(1) << 24);
-  // here p is an array indexed by strip
-  // first we initialize p for each strip, as above
-  uint32_t p[NUM_STRIPS];
-  for (int i = 0; i < NUM_STRIPS; i++) {
-    p[i] = pixels[i][pos] | MASK;
-  }
-  // then we send appropriate data to all strips as above
-  // toggling clock once we've sent a bit to all the strips
-  int j = 25;
-  while (j--) {
-    for (int i = 0; i < NUM_STRIPS; i++) {
-      digitalWrite(DATA_PIN + i, (p[i] & MASK) ? HIGH : LOW);
-      p[i] <<= 1;
-    }
-    toggle_clock();
-  }
-}
+// // write pixels of given index in strip layout from RGB array to all strips
+// // this has (theoretical) advantage that a single toggle_clock() pushes data down all strips at once
+// void ofxSM16716::write_pixel_all_strips(uint8_t pos) {
+//   // mask has a 1 in bit 25, zero everywhere else
+//   const uint32_t MASK = ((uint32_t)(1) << 24);
+//   // here p is an array indexed by strip
+//   // first we initialize p for each strip, as above
+//   uint32_t p[NUM_STRIPS];
+//   for (int i = 0; i < NUM_STRIPS; i++) {
+//     p[i] = pixels[i][pos] | MASK;
+//   }
+//   // then we send appropriate data to all strips as above
+//   // toggling clock once we've sent a bit to all the strips
+//   int j = 25;
+//   while (j--) {
+//     for (int i = 0; i < NUM_STRIPS; i++) {
+//       digitalWrite(DATA_PIN + i, (p[i] & MASK) ? HIGH : LOW);
+//       p[i] <<= 1;
+//     }
+//     toggle_clock();
+//   }
+// }
 
 // write a single blank pixel to all strips
 void ofxSM16716::write_blank_pixel() {
@@ -124,10 +123,9 @@ void ofxSM16716::write_blank_pixel() {
   // again, as above, we are writing 25 bits, since there is one bit between each set of 24 RGB bits
   int j = 25;
   while (j--) {
-    for (int k = 0; k < NUM_STRIPS; k++) {
-      // write a single 0 to each strip in turn
-      digitalWrite(DATA_PIN + k, 0);
-    }
+    
+    digitalWrite(DATA_PIN, 0);
+    
     toggle_clock();
   }
 }
@@ -140,53 +138,52 @@ void ofxSM16716::write_blank_pixel() {
 // we are writing the LEDs from the near end to the far end
 void ofxSM16716::show() {
   // write a single zero to each strip
-  for (int k = 0; k < NUM_STRIPS; k++) {
-    digitalWrite(DATA_PIN + k, LOW);
-  }
+  
+  digitalWrite(DATA_PIN, LOW);
+
   // toggle the clock 50 times
   for (int i = 0; i < 50; i++) {
     toggle_clock();
   }
   // cycle through the strips one at a time
-  for (int k = 0; k < NUM_STRIPS; k++) {
-    // and write each pixel in turn
-    for (int i = 0; i < LIGHT_COUNT; ++i) {
-      write_pixel(k, i);
-    }
+
+  for (int i = 0; i < pixels.size(); ++i) {
+    write_pixel(i);
   }
+  
   // write a single blank pixel
   write_blank_pixel();
   delay(1);
 }
 
 
-// push all pixels to grid doing strips concurrently
-// similar to show(), except we are using write_pixel_all_strips
-// so we do not also have to cycle through the strips
-void ofxSM16716::show_all_strips() {
-  for (int k = 0; k < NUM_STRIPS; k++) {
-  digitalWrite(DATA_PIN + k, LOW);
-  }
-  for (int i = 0; i < 50; i++) {
-    toggle_clock();
-  }
-  for (int i = 0; i < LIGHT_COUNT; ++i) {
-    write_pixel_all_strips(i);
-  }
-  write_blank_pixel();
-  delay(1);
-}
+// // push all pixels to grid doing strips concurrently
+// // similar to show(), except we are using write_pixel_all_strips
+// // so we do not also have to cycle through the strips
+// void ofxSM16716::show_all_strips() {
+  
+//   digitalWrite(DATA_PIN, LOW);
+
+//   for (int i = 0; i < 50; i++) {
+//     toggle_clock();
+//   }
+//   for (int i = 0; i < pixels.size(); ++i) {
+//     write_pixel_all_strips(i);
+//   }
+//   write_blank_pixel();
+//   delay(1);
+// }
 
 
     
 // clears LED grid by setting all the pixels in the array to zero
 // note: does not call show(), so does not actually display a blank grid
 void ofxSM16716::clear() {
-  for (int i = 0; i < NUM_STRIPS; i++) {
-    for (int j = 0; j< LIGHT_COUNT; j++) {
-      set_pixel_rgb(i, j, 0, 0, 0);
-    }
+  
+  for (int j = 0; j< pixels.size(); j++) {
+    set_pixel_rgb(j, 0, 0, 0);
   }
+
 }
 
 
--- src/ofxSM16716.h
+++ src/ofxSM16716.h
@@ -27,9 +27,6 @@
 // we will refer to this as the "strip layout"
 // the pixel array always corresponds the strip layout
 // number of strips
-#define NUM_STRIPS (4)
-// number of pixels per strip
-#define LIGHT_COUNT (400)
 
 
 
@@ -42,23 +39,23 @@ class ofxSM16716 {
     
     // this set of methods sets a pixel in the strip layout
     // set a pixel in the RGB array using a single 32-bit uint for the color; does not write to grid
-    void set_pixel(uint8_t strip, uint8_t index, uint32_t color);
+    void set_pixel(uint8_t index, uint32_t color);
 
-    bool init();
+    bool init(int num_pix);
     
     // set a pixel in the RBG array using separate uints for R, G, and B; does not write to grid
-    void set_pixel_rgb(uint8_t strip, uint8_t index, uint32_t r, uint32_t g, uint32_t b);
+    void set_pixel_rgb(uint8_t index, uint32_t r, uint32_t g, uint32_t b);
    
     // this set of methods writes pixels from the pixel array to the LED strips
     // toggle clock pin high then low; "pushes" each bit one bit down the strip
     void toggle_clock();
     
     // write a single pixel of given index in strip layout from RGB array to a given strip
-    void write_pixel(uint8_t strip, uint8_t i);
+    void write_pixel(uint8_t i);
     
-    // write pixels of given index in strip layout from RGB array to all strips
-    // this has (theoretical) advantage that a single toggle_clock() pushes data down all strips at once
-    void write_pixel_all_strips(uint8_t pos);
+    // // write pixels of given index in strip layout from RGB array to all strips
+    // // this has (theoretical) advantage that a single toggle_clock() pushes data down all strips at once
+    // void write_pixel_all_strips(uint8_t pos);
     
     // write a blank pixel to all strips
     void write_blank_pixel();
@@ -67,7 +64,7 @@ class ofxSM16716 {
     void show();
     
     // push all pixels from array to grid doing strips concurrently; uses write_pixel_all_strips
-    void show_all_strips();
+    // void show_all_strips();
     
     // clear the entire LED grid; does not call show()
     void clear();
@@ -75,7 +72,9 @@ class ofxSM16716 {
   private:
     // array containing the RGB grid
     // this represents the 
-    uint32_t pixels[NUM_STRIPS][LIGHT_COUNT];
+    // uint32_t pixels[LIGHT_COUNT];
+    vector <uint32_t> pixels;
+
  };
  
  #endif
